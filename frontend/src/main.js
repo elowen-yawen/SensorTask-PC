8929e1e52bb154cf3e3cf8ea361de571d387612c
Elowen <3376027149@qq.com>
2026-05-14 19:31:08 +0800

    5.14

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,4 +13,4 @@ app.mount('#app')
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
-}    
+}          
